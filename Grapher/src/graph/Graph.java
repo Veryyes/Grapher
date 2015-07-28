29972eda047b88be6d23b61b18b3b544c20f7dc8
Veryyes <Veryyes10@yahoo.com>
2015-07-27 21:44:22 -0400

    Added graph()

--- Grapher/src/graph/Graph.java
+++ Grapher/src/graph/Graph.java
@@ -11,7 +11,7 @@ import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Graph extends JPanel {
-	public static LinkedList<Point2D.Float> points;
+	public static LinkedList<Point2D.Float> points =  new LinkedList<Point2D.Float>();
 	public static LinkedList<Term> equation;
 	public static int xScale=10;
 	public static int yScale=1;
@@ -19,7 +19,9 @@ public class Graph extends JPanel {
 	public static int xMin=-500;
 	public static int yMax=600;
 	public static int yMin=-600;
-	public static int ptSize = 5;
+	public static float xStep=1;
+	public static float yStep=1;
+	public static int ptSize = 6;
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Grapher");
 		frame.setSize(Math.abs(xMax-xMin),Math.abs(yMax-yMin));
@@ -31,10 +33,9 @@ public class Graph extends JPanel {
 		equation = new LinkedList<Term>();
 		equation.add(new Term(1,3));
 		equation.add(new Term(-3,2));
-		equation.add(new Term(-13,1));
+		equation.add(new Term(2,1));
 		equation.add(new Term(15,0));
 		
-		points = new LinkedList<Point2D.Float>();
 		for(int x = xMin; x<=xMax;x++){
 			float sum = 0;
 			for(int i=0;i<equation.size();i++){
@@ -43,12 +44,46 @@ public class Graph extends JPanel {
 			points.add(new Point2D.Float(x,(int) sum*-1));
 		}
 	}
+	public static void graph(LinkedList<Point2D.Float> points){
+		Graph.points=points;
+		
+		JFrame frame = new JFrame("Grapher");
+		frame.setSize(Math.abs(xMax-xMin),Math.abs(yMax-yMin));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		Graph graph = new Graph();
+		frame.add(graph);
+		
+	}
+	public static void graph(LinkedList<Term> equation, float dx){
+		for(int x = xMin; x<=xMax;x+=dx){
+			float sum = 0;
+			for(int i=0;i<equation.size();i++){
+				sum+=equation.get(i).getValue(x);
+			}
+			points.add(new Point2D.Float(x,(int) sum*-1));
+		}
+		graph(points);
+	}
+	public static void graph(LinkedList<Term> equation, int xMin, int xMax, float dx){
+		Graph.xMin=xMin;
+		Graph.xMax=xMax;
+		for(int x = Graph.xMin; x<=Graph.xMax;x+=dx){
+			float sum = 0;
+			for(int i=0;i<equation.size();i++){
+				sum+=equation.get(i).getValue(x);
+			}
+			points.add(new Point2D.Float(x,(int) sum*-1));
+		}
+		graph(points);
+	}
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		repaint();
 		g.setColor(Color.BLACK);
 		g.drawLine(0, yMax, Math.abs(xMax-xMin), yMax); //x-axis
 		g.drawLine(Math.abs(xMin),0,Math.abs(xMin),Math.abs(yMax-yMin)); //y-axis
+		g.setColor(Color.BLACK);
 		if(points!=null){
 			for(int i=0;i<points.size()-1;i++){
 				g.fillOval((int)((points.get(i).x*xScale-(ptSize/2))+Math.abs(xMin)), (int)(points.get(i).y*yScale-(ptSize/2)+Math.abs(yMax)), ptSize, ptSize);
